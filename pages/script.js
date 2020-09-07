8968d10b154c8ff5627bfd14f7eb68a8f8bc5d07
Kseniya Martinkaite <ksumartinkaite@gmail.com>
2020-09-07 03:30:20 +0300

    five sprint

--- pages/script.js
+++ pages/script.js
@@ -2,35 +2,34 @@ const userName = document.querySelector('.profile__title');
 const userJob = document.querySelector('.profile__text')
 const nameInput = document.querySelector('.nameinput');
 const jobInput = document.querySelector('.jobinput');
-// popupadd
+
 const nameMesto = document.querySelector('.namemesto');
 const imageMesto = document.querySelector('.imagemesto');
-//const popup = document.querySelector('.popup');
+
 const forms = document.forms;
-// edit profile popup
+
 const popupEdit = document.querySelector('.popup_edit');
-// add card popup
+
 const popupAdd = document.querySelector('.popup_addcard');
-//const popups = document.querySelectorAll('.profile');
-// edit profile form
+
 const profileForm = document.querySelector('.profileform');
-// add card form
+
 const cardForm = document.querySelector('.cardform');
-// card popup button functionality
+
 const cardButton = document.querySelector('.profile__add')
-    // open popup functionality
+
 const editButton = document.querySelector('.profile__edit');
-// close popup functionality
+
 const closePopup = document.querySelector('.popup__close');
-// card close functionality
+
 const cardCloseButton = document.querySelector('#cardCloseButton');
-// image popup functionality
+
 const elementPopup = document.querySelector('.elementpopup');
 const elementPopupImage = document.querySelector('.elementpopup__image');
 const elementPopupText = document.querySelector('.elementpopup__text');
 const elementPopupClose = document.querySelector('.elementpopup__close');
 
-// объявляем перменную для добавления карточек на страницу при загрузке;
+
 const initialCards = [{
         name: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -57,12 +56,11 @@ const initialCards = [{
     }
 ];
 
-//объявляем переменную для template
+
 const elementTemplate = document.querySelector('.element-template').content;
-//объявляем переменную куда буду вставлять template
+
 const elementSection = document.querySelector('.elements');
 
-//popup profile
 
 function setProfileInfo() {
     nameInput.value = userName.textContent;
@@ -127,7 +125,6 @@ function addCard(name, link, mod) {
     img.alt = name;
     img.addEventListener('click', function() {
         elementPopup.classList.toggle('elementpopup_opened');
-        //elementPopup.style.display = 'flex';
         elementPopupImage.src = this.src;
         elementPopupText.textContent = this.alt;
     });
@@ -142,7 +139,7 @@ function addCard(name, link, mod) {
 // инициализируем карточ по умолчанию
 initialCards.forEach(card => addCard(card.name, card.link, "initial"));
 
-//elementLikeButton.addEventListener('click', function() { elementLikeButton.classList.toggle('element__like_active'); });
+
 elementPopupClose.addEventListener('click', function() { elementPopup.classList.toggle('elementpopup_opened'); });
 editButton.addEventListener('click', function() { togglePopup('popup_edit'); });
 closePopup.addEventListener('click', function() { togglePopup('popup_edit'); });
